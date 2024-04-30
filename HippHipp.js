62b138cedfef46edcd431d75b201040c76d13c2f
krispypatata <keithginoel@gmail.com>
2024-04-30 20:56:12 +0800

    easy kattis problem

---
+++ HippHipp.js
@@ -0,0 +1,17 @@
+/*
+@author Keith Ginoel S. Gabinete
+@date 2024-04-29 00:46:35
+*/
+/*
+Forritunarkeppni Framhaldsskólanna was first held in the year 2001. 
+This year the contest is celebrating a big anniversary, being 20 years old! Shouldn’t we congratulate?
+Input
+
+There is no input in this problem.
+Output
+
+Print 20 lines. Each line should contain Hipp hipp hurra!
+*/
+
+for (let i = 0; i < 20; i++)
+    console.log('Hipp hipp hurra!');
---
+++ TilHamingju.js
@@ -0,0 +1,16 @@
+/*
+@author Keith Ginoel S. Gabinete
+@date 2024-04-29 00:50:59
+*/
+/*
+Forritunarkeppni Framhaldsskólanna was first held in the year 2001. 
+This year there’s a big anniversary because it’s 20 years old! Shouldn’t we congratulate it?
+Input
+
+There is no input in this problem.
+Output
+
+Print a single line containing TIL HAMINGJU MED AFMAELID FORRITUNARKEPPNI FRAMHALDSSKOLANNA!
+*/
+
+console.log('TIL HAMINGJU MED AFMAELID FORRITUNARKEPPNI FRAMHALDSSKOLANNA!');
